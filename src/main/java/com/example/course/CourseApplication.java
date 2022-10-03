62615682e3aa774e88a7a06fc2bd01d8bf0ac1df
Tiago Ribolli <ribollitiago@gmail.com>
2022-10-03 19:36:59 -0300

    User entity and resource

--- src/main/java/com/example/course/CourseApplication.java
+++ src/main/java/com/example/course/CourseApplication.java
@@ -3,11 +3,13 @@ package com.example.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.course.entities.User;
+
 @SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+		
 	}
-
 }
